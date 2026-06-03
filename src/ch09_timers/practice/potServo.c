2e94dd47cd372ed9ab5a5df3d66a2d65a50ba9f3
Salzu <thesalzuu@gmail.com>
2026-06-02 23:11:10 -0400

    ADC tricks
    Learned some cool ADC tricks that include moving average and oversampling. Also more confident with setting registers to turn on the ADC registry

--- src/ch09_timers/practice/potServo.c
+++ src/ch09_timers/practice/potServo.c
@@ -1,43 +1,43 @@
-// Definitions & Includes
-#include <avr/io.h>
-#include "analogRead.h"
-#include "hal.h"
+// // Definitions & Includes
+// #include <avr/io.h>
+// #include "analogRead.h"
+// #include "hal.h"
 
-#define PULSE_MIN 2000 
-#define PULSE_MID 3000
-#define PULSE_MAX 4000
+// #define PULSE_MIN 2000 
+// #define PULSE_MID 3000
+// #define PULSE_MAX 4000
 
-#define SERVO PB1
-#define SERVO_DDR DDRB
+// #define SERVO PB1
+// #define SERVO_DDR DDRB
 
-#define POT PC0
+// #define POT PC0
 
-// Function Definitions
-void initTimer(void);
+// // Function Definitions
+// void initTimer(void);
 
-// Main Code
-int main(void) {
-  initTimer();
-  initADC();
+// // Main Code
+// int main(void) {
+//   initTimer();
+//   initADC();
 
-  OCR1A = PULSE_MID; // start at center position
+//   OCR1A = PULSE_MID; // start at center position
 
-  // Main Loop
-  while (1) {
-    const uint16_t potValue = readADC(POT);
-    OCR1A = map(potValue, ANALOG_MIN, ANALOG_MAX, PULSE_MIN, PULSE_MAX);
-  }
+//   // Main Loop
+//   while (1) {
+//     const uint16_t potValue = readADC(POT);
+//     OCR1A = map(potValue, ANALOG_MIN, ANALOG_MAX, PULSE_MIN, PULSE_MAX);
+//   }
 
-  return 0;
-}
+//   return 0;
+// }
 
-// Functions
-void initTimer(void) {
-  TCCR1A |= (1 << COM1A1);               // Non-inverting PWM
-  TCCR1A |= (1 << WGM11);                // Fast PWM part (1st bit needed)
-  TCCR1B |= (1 << WGM13) | (1 << WGM12); // Fast PWM with ICR1 as TOP (last 2 bits needed)
-  TCCR1B |= (1 << CS11);                 // /8 prescaler for the most definition
-  ICR1 = 40000;                          // 20ms, 50hz per cycle
+// // Functions
+// void initTimer(void) {
+//   TCCR1A |= (1 << COM1A1);               // Non-inverting PWM
+//   TCCR1A |= (1 << WGM11);                // Fast PWM part (1st bit needed)
+//   TCCR1B |= (1 << WGM13) | (1 << WGM12); // Fast PWM with ICR1 as TOP (last 2 bits needed)
+//   TCCR1B |= (1 << CS11);                 // /8 prescaler for the most definition
+//   ICR1 = 40000;                          // 20ms, 50hz per cycle
 
-  SERVO_DDR |= (1 << SERVO); // Set servo pin to output
-}
+//   SERVO_DDR |= (1 << SERVO); // Set servo pin to output
+// }
