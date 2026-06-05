f1683ca1d4eaafa6c1edb4ab396c8a8ff2ea4b44
Salzu <thesalzuu@gmail.com>
2026-06-05 02:36:37 -0400

    Transistors And PWM-Tricks
    Learned some cool PWM depth tricks along with how Ttransistors work.

--- src/ch12_adc-conversion2/potReadingConversions.c
+++ src/ch12_adc-conversion2/potReadingConversions.c
@@ -1,96 +1,96 @@
-// Includes/Definitions
-#include <avr/io.h>
-#include <avr/sleep.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-#include "USART.h"
+// // Includes/Definitions
+// #include <avr/io.h>
+// #include <avr/sleep.h>
+// #include <avr/interrupt.h>
+// #include <util/delay.h>
+// #include "USART.h"
 
-#define POT_PIN PC0
-#define WINDOW_SIZE 4
+// #define POT_PIN PC0
+// #define WINDOW_SIZE 4
 
-// Typedefs
-typedef struct {
-  uint16_t totalValue;
-  uint8_t lastModifiedIndex;
-  uint16_t contents[WINDOW_SIZE];
-} Buffer;
+// // Typedefs
+// typedef struct {
+//   uint16_t totalValue;
+//   uint8_t lastModifiedIndex;
+//   uint16_t contents[WINDOW_SIZE];
+// } Buffer;
 
-// Function Definitions
-void initADC();
-uint16_t readAnalog();
-uint16_t oversample16x();
-uint16_t getMovingAverage(uint16_t newSample, Buffer *pBuffer);
+// // Function Definitions
+// void initADC();
+// uint16_t readAnalog();
+// uint16_t oversample16x();
+// uint16_t getMovingAverage(uint16_t newSample, Buffer *pBuffer);
 
-// Interrupts
-EMPTY_INTERRUPT(ADC_vect);
+// // Interrupts
+// EMPTY_INTERRUPT(ADC_vect);
 
-// Main Code
-int main(void) {
-  initADC();
-  initUSART();
-  sei();
+// // Main Code
+// int main(void) {
+//   initADC();
+//   initUSART();
+//   sei();
 
-  // Variables
-  Buffer movingAverageBuffer = {0};
+//   // Variables
+//   Buffer movingAverageBuffer = {0};
 
-  // Main loop
-  while (1) {
-    const uint16_t rawValue = readAnalog();
-    const uint16_t oversampledPotValue = oversample16x();
-    const uint16_t movingAverage = getMovingAverage(oversampledPotValue, &movingAverageBuffer);
+//   // Main loop
+//   while (1) {
+//     const uint16_t rawValue = readAnalog();
+//     const uint16_t oversampledPotValue = oversample16x();
+//     const uint16_t movingAverage = getMovingAverage(oversampledPotValue, &movingAverageBuffer);
 
-    printString("Raw Value: ");
-    printWord(rawValue);
-    printString("\r\n");
+//     printString("Raw Value: ");
+//     printWord(rawValue);
+//     printString("\r\n");
 
-    printString("Oversampled Value: ");
-    printWord(oversampledPotValue);
-    printString("\r\n");
+//     printString("Oversampled Value: ");
+//     printWord(oversampledPotValue);
+//     printString("\r\n");
 
-    printString("Moving Average: ");
-    printWord(movingAverage);
-    printString("\r\n");
+//     printString("Moving Average: ");
+//     printWord(movingAverage);
+//     printString("\r\n");
 
-    _delay_ms(3000);
-  }
+//     _delay_ms(3000);
+//   }
 
-  return 0;
-}
+//   return 0;
+// }
 
-// Functions
-void initADC() {
-  ADMUX |= (1 << REFS0); // Use the 0-5v voltage from the board for ref voltage
-  ADMUX = (ADMUX & 0xF0) | POT_PIN; // Tell the multiplexer which pin is getting read
-  ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // 128 prescaler
-  ADCSRA |= (1 << ADIE); // Enables the intterup to wake the cpu up when the adc conversion is done
-  ADCSRA |= (1 << ADEN); // Enable the ADC
+// // Functions
+// void initADC() {
+//   ADMUX |= (1 << REFS0); // Use the 0-5v voltage from the board for ref voltage
+//   ADMUX = (ADMUX & 0xF0) | POT_PIN; // Tell the multiplexer which pin is getting read
+//   ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // 128 prescaler
+//   ADCSRA |= (1 << ADIE); // Enables the intterup to wake the cpu up when the adc conversion is done
+//   ADCSRA |= (1 << ADEN); // Enable the ADC
 
-  set_sleep_mode(SLEEP_MODE_ADC);
-}
+//   set_sleep_mode(SLEEP_MODE_ADC);
+// }
 
-uint16_t readAnalog() {
-  ADCSRA &= ~(1 << ADIE); // Disable the interrupt
-  ADCSRA |= (1 << ADSC); // Start the read
-  while (ADCSRA & (1 << ADSC)) {}
-  ADCSRA |= (1 << ADIE); // ENable the interrupt
-  return ADC;
-}
+// uint16_t readAnalog() {
+//   ADCSRA &= ~(1 << ADIE); // Disable the interrupt
+//   ADCSRA |= (1 << ADSC); // Start the read
+//   while (ADCSRA & (1 << ADSC)) {}
+//   ADCSRA |= (1 << ADIE); // ENable the interrupt
+//   return ADC;
+// }
 
-uint16_t oversample16x() {
-  uint16_t oversampledValue = 0;
+// uint16_t oversample16x() {
+//   uint16_t oversampledValue = 0;
 
-  for (uint8_t i = 0; i < 16; i++) {
-    sleep_mode();
-    oversampledValue += ADC;
-  }
+//   for (uint8_t i = 0; i < 16; i++) {
+//     sleep_mode();
+//     oversampledValue += ADC;
+//   }
 
-  return oversampledValue >> 2; // shift right 2 = divide by 4 = drop 2 noisy bits
-}
+//   return oversampledValue >> 2; // shift right 2 = divide by 4 = drop 2 noisy bits
+// }
 
-uint16_t getMovingAverage(uint16_t newSample, Buffer *pBuffer) {
-  pBuffer->lastModifiedIndex = (pBuffer->lastModifiedIndex + 1) % WINDOW_SIZE; // set next index
-  pBuffer->totalValue -= pBuffer->contents[pBuffer->lastModifiedIndex]; // remove oldest
-  pBuffer->contents[pBuffer->lastModifiedIndex] = newSample; // replace with new
-  pBuffer->totalValue += newSample; // add new to total
-  return pBuffer->totalValue / WINDOW_SIZE; // return average
-}
+// uint16_t getMovingAverage(uint16_t newSample, Buffer *pBuffer) {
+//   pBuffer->lastModifiedIndex = (pBuffer->lastModifiedIndex + 1) % WINDOW_SIZE; // set next index
+//   pBuffer->totalValue -= pBuffer->contents[pBuffer->lastModifiedIndex]; // remove oldest
+//   pBuffer->contents[pBuffer->lastModifiedIndex] = newSample; // replace with new
+//   pBuffer->totalValue += newSample; // add new to total
+//   return pBuffer->totalValue / WINDOW_SIZE; // return average
+// }
---
+++ src/ch14_dc-motor/switch.c
@@ -0,0 +1,23 @@
+// Includes/Definitions
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define SWITCH PB1
+#define SWITCH_DDR DDRB
+#define SWITCH_PORT PORTB
+
+// This was more so a hardware thing. I connected up a transitor with a diode to run a motor
+
+int main(void) {
+
+  SWITCH_DDR |= (1 << SWITCH); // Output mode
+
+  while (1) {
+    SWITCH_PORT |= (1 << SWITCH);
+    _delay_ms(1000);
+    SWITCH_PORT &= ~(1 << SWITCH);
+    _delay_ms(1000);
+  }
+
+  return 0;
+}
